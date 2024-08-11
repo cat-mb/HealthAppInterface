53226be503c94b694ad2d3d7e478408cccb5b442
Cat <catalinabarnes04@gmail.com>
2024-08-11 15:28:16 -0700

    Initial Commit

---
+++ HealthAppInterface/HealthAppInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAppInterfaceApp.swift
+//  HealthAppInterface
+//
+//  Created by Catalina Barnes on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthAppInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
